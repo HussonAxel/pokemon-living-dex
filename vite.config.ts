df376ee0385b4b3b2a46c0ad0faaab6dbb014ccd
axhus <axel.husson@smile.fr>
2025-10-25 18:42:00 +0200

    [chore] update Netlify configuration to use Vite for build and dev commands, add Netlify plugin for TanStack

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import neon from './neon-vite-plugin.ts'
 
 const config = defineConfig({
@@ -14,6 +15,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact(),
   ],
 })
