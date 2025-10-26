92975e3c0fd9ca26cea40837cd2a424371ff0ab3
axhus <axel.husson@smile.fr>
2025-10-26 23:57:57 +0100

    [chore] add babel-plugin-react-compiler and tanstack-router-sitemap; update Vite config to use babel plugin for React

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 import neon from './neon-vite-plugin.ts'
+
 
 const config = defineConfig({
   plugins: [
@@ -16,7 +17,12 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     netlify(),
-    viteReact(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+
   ],
 })
 
