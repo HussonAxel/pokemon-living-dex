3d6133ad0b7d1d6cc0a4e1078fc7c4282520bbda
axhus <axel.husson@smile.fr>
2025-10-26 16:31:21 +0100

    [style] update GridHeaderMenu layout and styles; adjust margins and grid structure for improved responsiveness

--- src/components/gridHeaderMenu.tsx
+++ src/components/gridHeaderMenu.tsx
@@ -9,8 +9,8 @@ export default function GridHeaderMenu({}: gridHeaderMenuProps) {
 
   return (
     <nav className="h-screen">
-      <div className="flex flex-col md:h-full md:gap-4 xl:flex-row mx-12">
-        <div className="w-full lg:w-[550px] xl:w-[700px] flex-shrink-0 mt-12 lg:my-12">
+      <div className="flex flex-col md:h-full md:gap-4 xl:flex-row mx-4 md:mx-12">
+        <div className="w-full xl:w-[700px] flex-shrink-0 mb-8 md:mb-0 mt-12 xl:my-12">
           <Link
             to={'/'}
           >
@@ -29,21 +29,19 @@ export default function GridHeaderMenu({}: gridHeaderMenuProps) {
             </div>
           </Link>
         </div>
--
-        <div className="flex-1 overflow-y-auto scrollbar-hide mt-12 lg:my-12">
-          <div className="grid grid-cols-3 gap-6 auto-rows-fr">
+        <div className="flex-1 overflow-y-auto scrollbar-hide xl:my-12">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 auto-rows-fr">
             {headerLinks.slice(1).map((link) => (
               <Link
                 to={link.slug}
                 onMouseEnter={usePrefetchAllBerriesWithData()}
               >
-                <div className="border-[0.5px] border-black rounded-[20px] p-6 transition-colors duration-500 ease-in-out hover:bg-[#ef4036] hover:text-white hover:cursor-pointer flex flex-col h-80">
+                <div className="border-[0.5px] border-black rounded-[20px] p-6 transition-colors duration-500 ease-in-out hover:bg-[#ef4036] hover:text-white hover:cursor-pointer flex flex-col h-40 md:h-80">
                   <div className="flex flex-col justify-between h-full">
                     <div className="flex flex-row justify-between">
                       <p className="font-bold text-2xl">{link.name}</p>
                     </div>
                     <div className="flex flex-col gap-6">
-                      <p className="text-3xl font-bold">{link.name}</p>
                       <p className="text-md leading-relaxed flex-1 overflow-y-auto font-semibold">
                         {link.description}
                       </p>
