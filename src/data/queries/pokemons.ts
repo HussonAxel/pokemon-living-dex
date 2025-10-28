8b54c31f780e9092bbaed953c58a2b0f6d233b07
axhus <axel.husson@smile.fr>
2025-10-28 13:22:27 +0100

    [feat] try smthng

--- src/data/queries/pokemons.ts
+++ src/data/queries/pokemons.ts
@@ -31,7 +31,7 @@ const STATIC_QUERY_OPTIONS = {
 //
 
 export const fetchAllAbilities = async () => {
-const res = await fetch(`${BASE_POKEAPI_URL}/ability?limit=-1`)
+const res = await fetch(`${BASE_POKEAPI_URL}/ability?limit=50`)
 if (!res.ok) {
   throw new Error('Failed to fetch abilities')
 }
